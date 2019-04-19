191c2716aa261bf3e18b8a7dd915e15d311f62d0
Kaung Myat Han <arkarhtethan@gmail.com>
2019-04-19 21:49:07 +0630

    complete basic rest api

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from . import routers
 
 app_name="product"
 
@@ -10,5 +11,6 @@ urlpatterns = [
 	path('create/',views.ProductCreateView.as_view(),name='create'),
 	path('update/<int:pk>/',views.ProductUpdateView.as_view(),name='update'),
 	path('delete/<int:pk>/',views.ProductDeleteView.as_view(),name='delete'),
+	path('api/',include(routers.router.urls)),
 
 ]
